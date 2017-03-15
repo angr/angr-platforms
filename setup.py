67ba1ec3052e3e197cdc71b4d1ad98575239fa56
Eric Gustafson <edg@cs.ucsb.edu>
2017-03-14 19:26:15 -0700

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='angr_bf',
+    version='0.1',
+    description='This is a demonstration of how to extend angr. This module allows for the symoblic exeution of the esoteric language Branfuck.',
+    packages=['angr_bf'],
+    install_requires=[
+        'angr',
+        'simuvex',
+        'cle',
+        'archinfo',
+        'pyvex',
+    ],
+)
