540fb856fa3028625dc6d2c27cf35e7b5e282c1b
Yan <yans@yancomm.net>
2017-03-23 16:11:31 -0700

    dash instead of _

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='angr_bf',
+    name='angr-bf',
     version='0.1',
     description='This is a demonstration of how to extend angr. This module allows for the symoblic exeution of the esoteric language Branfuck.',
     packages=['angr_bf'],
