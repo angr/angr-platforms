c23adcce81824bb5d073a5679ef5aaddc57c3a32
Eric Gustafson <edg@cs.ucsb.edu>
2017-09-08 15:13:30 -0700

    Fix file paths

--- tests/test_angr_bf.py
+++ tests/test_angr_bf.py
@@ -10,7 +10,7 @@ def test_hello():
     End-to-end Hello World path analysis
     :return:
     """
-    hellobf = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../test_programs/hello.bf'))
+    hellobf = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../test_programs/bf/hello.bf'))
     p = angr.Project(hellobf)
     entry = p.factory.entry_state()
     smgr = p.factory.simgr(entry)
@@ -22,7 +22,7 @@ def test_1bytecrackme_good():
     The world-famous 1-byte crackme (easy version)
     :return:
     """
-    crackme = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../test_programs/1bytecrackme-good.bf'))
+    crackme = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../test_programs/bf/1bytecrackme-good.bf'))
     bad_states = lambda state: "-" in state.posix.dumps(1)
     p = angr.Project(crackme)
     entry = p.factory.entry_state(remove_options={angr.options.LAZY_SOLVES})
