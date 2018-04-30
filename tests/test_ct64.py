104f0d8a4596232b384c68cc352638ab26c13a87
Audrey Dutcher <audrey@rhelmot.io>
2018-04-30 12:51:47 -0700

    Split ct64 test into fast/slow

--- tests/test_ct64.py
+++ tests/test_ct64.py
@@ -1,11 +1,22 @@
 import logging
 import os
 
+from nose.plugins.attrib import attr
 from angr_platforms import ct64
 
 def deinterlace(s):
     return ''.join(x for i, x in enumerate(s) if i % 2 == 1)
 
+def test_quick_ct64():
+    p = ct64.load_rom(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../test_programs/ct64/distribute.rom'))
+    simgr = p.factory.simulation_manager()
+    simgr.run(n=100)
+    assert len(simgr.active) == 6
+    for active in simgr.active:
+        assert active.posix.dumps(0) != ''
+        assert deinterlace(active.posix.dumps(1)) == 'PASSWORD: '
+
+@attr(speed='slow')
 def test_crackme():
     p = ct64.load_rom(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../test_programs/ct64/distribute.rom'))
 
@@ -15,8 +26,9 @@ def test_crackme():
 
     simgr = p.factory.simulation_manager()
     simgr.explore(
-        find=lambda s: 'flag{' in deinterlace(s.posix.dumps(1)),
-        )#step_func=lambda lsm: lsm.drop(stash='deadended'))
+        avoid=[0x12cc, 0x1316, 0x1338, 0x14c9],
+        find=0x1608,
+        step_func=lambda lsm: lsm.drop(stash='deadended'))
 
     if not simgr.found:
         assert False, "Failed to find any path containing the flag"
@@ -24,5 +36,6 @@ def test_crackme():
     print repr(out)
 
 if __name__ == '__main__':
-    logging.getLogger('angr.manager').setLevel('DEBUG')
+    logging.getLogger('angr.sim_manager').setLevel('DEBUG')
+    test_quick_ct64()
     test_crackme()
