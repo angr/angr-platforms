e12e6018ae7a07933e46e7365168baa695703ee5
Audrey Dutcher <audrey@rhelmot.io>
2019-07-14 17:02:55 -0700

    kosher arch creation

--- angr_platforms/bpf/arch_bpf.py
+++ angr_platforms/bpf/arch_bpf.py
@@ -13,26 +13,28 @@ class ArchBPF(Arch):
     def __init__(self, endness="Iend_LE"):
         super(ArchBPF, self).__init__('Iend_LE')
 
-        self.bits = 32
-        self.vex_arch = None
-        self.name = "BPF"
-        self.instruction_endness = 'Iend_BE'
-        self.memory_endness = 'Iend_LE'
+    bits = 32
+    vex_arch = None
+    name = "BPF"
+    instruction_endness = 'Iend_BE'
+    memory_endness = 'Iend_LE'
 
-        self.registers = { }
-        self.registers['A'] = (0, 4)
-        self.registers['X'] = (4, 4)
-        self.registers['pc'] = (8, 4)
-        self.registers['ip'] = (8, 4)
-        self.registers['res'] = (12, 4)
+    registers = {
+        'A': (0, 4),
+        'X': (4, 4),
+        'pc': (8, 4),
+        'ip': (8, 4),
+        'res': (12, 4)
+    }
 
-        self.register_names = { }
-        self.register_names[0] = 'A'
-        self.register_names[4] = 'X'
-        self.register_names[8] = 'pc'
-        self.register_names[12] = 'res'
+    register_names = {
+        0: 'A',
+        4: 'X',
+        8: 'pc',
+        12: 'res'
+    }
 
-        self.ip_offset = self.registers["pc"][0]
+    ip_offset = registers["pc"][0]
 
 
 register_arch(['bpf'], 32, 'any', ArchBPF)
