39b7a34e43320310294058353f341fc0f17d362f
Audrey Dutcher <audrey@rhelmot.io>
2019-10-25 14:40:39 -0700

    Fix everything except the bf lifter

--- tests/test_engine_bf.py
+++ tests/test_engine_bf.py
@@ -9,6 +9,7 @@ from angr_platforms.bf.engine_bf import UberEngineWithBF
 def test_hello():
     hellobf = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../test_programs/bf/hello.bf'))
     p = angr.Project(hellobf, engine=UberEngineWithBF)
+    p.arch.vex_arch = None  # force test with engine
     entry = p.factory.entry_state()
     smgr = p.factory.simulation_manager(entry)
     smgr.explore()
@@ -17,7 +18,8 @@ def test_hello():
 def test_1bytecrackme_good():
     crackme = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../test_programs/bf/1bytecrackme-good.bf'))
     bad_states = lambda state: b"-" in state.posix.dumps(1)
-    p = angr.Project(crackme)
+    p = angr.Project(crackme, engine=UberEngineWithBF)
+    p.arch.vex_arch = None  # force test with engine
     entry = p.factory.entry_state(remove_options={angr.options.LAZY_SOLVES})
     smgr = p.factory.simulation_manager(entry)
     smgr.run(until=lambda lsmgr: len(lsmgr.active) == 0)
