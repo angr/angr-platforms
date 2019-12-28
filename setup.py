f95e2694d4f5df79e435cccb849b0f433da4de52
Whatang <mike@whatang.org>
2019-12-28 21:18:42 +0000

    Install sub-modules with pip by using find_packages.
    Sub-modules (e.g. msp430) are not found when angr-platforms is installed with pip, because only the top-level module is listed.
    Using setuptools' find_packages function corrects this.
    
    Fixes #30.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='angr_platforms',
     version='0.1',
     description='A collection of extra platforms for angr',
-    packages=['angr_platforms'],
+    packages=find_packages(),
     install_requires=[
         'angr',
         'cle',
