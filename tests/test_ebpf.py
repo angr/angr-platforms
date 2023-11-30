7211e701d6b52b65dab29eb799606574cc707e75
Kevin Phoenix <kevin@kphoenix.us>
2023-11-30 12:51:55 -0700

    Fix test_prog_always_return_42 from erroring in pytest

--- tests/test_ebpf.py
+++ tests/test_ebpf.py
@@ -8,28 +8,28 @@ from angr_platforms.ebpf import ArchExtendedBPF, LifterEbpf
 TEST_PROGRAMS_BASE = Path(__file__).parent.parent / "test_programs" / "ebpf"
 
 
-def test_prog_always_returns_42(filename: str) -> None:
-    proj = angr.Project(TEST_PROGRAMS_BASE / filename)
-    assert isinstance(proj.arch, ArchExtendedBPF)
-
-    state = proj.factory.entry_state()
-    simgr = proj.factory.simgr(state)
-    simgr.run()
-
-    assert len(simgr.deadended) == 1
-    assert state.solver.eval_exact(simgr.deadended[0].regs.R0, 1) == [42]
-
-
 class TestEbpf(unittest.TestCase):
+    @staticmethod
+    def _test_prog_always_returns_42(filename: str) -> None:
+        proj = angr.Project(TEST_PROGRAMS_BASE / filename)
+        assert isinstance(proj.arch, ArchExtendedBPF)
+
+        state = proj.factory.entry_state()
+        simgr = proj.factory.simgr(state)
+        simgr.run()
+
+        assert len(simgr.deadended) == 1
+        assert state.solver.eval_exact(simgr.deadended[0].regs.R0, 1) == [42]
+
     # pylint:disable=missing-class-docstring,no-self-use
     def test_trivial_return(self):
-        test_prog_always_returns_42("return_42.o")
+        self._test_prog_always_returns_42("return_42.o")
 
     def test_branched_return(self):
-        test_prog_always_returns_42("return_if.o")
+        self._test_prog_always_returns_42("return_if.o")
 
     def test_get_ns(self):
-        test_prog_always_returns_42("get_ns.o")
+        self._test_prog_always_returns_42("get_ns.o")
 
     def test_ebpf_lift(self):
         proj = angr.Project(TEST_PROGRAMS_BASE / "return_42.o")
