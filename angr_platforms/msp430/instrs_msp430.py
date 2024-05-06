f07fa482f3a6f8e7c8aca7f6d05088473e14cd6f
Jo Van Bulck <jovanbulck@users.noreply.github.com>
2024-05-06 21:45:45 +0200

    msp430: fix BR instruction behavior and register-indirect @Rn disassembly (#65)
    * msp430: fix BR instruction
    
    Current behavior erroneously jumps to the _address_ of the BR instruction,
    instead of the SRC operand. This leads to an infinite loop.
    
    * msp430: fix disassembly register-indirect @Rn mode
    
    Fixes:
    "UnboundLocalError: local variable 'reg_str' referenced before assignment"

--- angr_platforms/msp430/instrs_msp430.py
+++ angr_platforms/msp430/instrs_msp430.py
@@ -330,7 +330,7 @@ class MSP430Instruction(Instruction):
             reg_str = "%d(%s)" % (imm, reg_name)
         # Indirect mode; fetch address in register; store is a write there.
         elif reg_mode == ArchMSP430.Mode.INDIRECT_REGISTER_MODE:
-            reg_str = "@%s" % reg_str
+            reg_str = "@%s" % reg_name
         # Indirect Autoincrement mode. Increment the register by the type size, then access it
         elif reg_mode == ArchMSP430.Mode.INDIRECT_AUTOINCREMENT_MODE:
             reg_str = "@%s+" % reg_name
@@ -648,7 +648,7 @@ class Instruction_MOV(Type3Instruction):
                 self.jump(None, newpc, jumpkind=JumpKind.Ret)
             else:
                 # If we're setting PC, but not from SP+, it's a BR instead
-                self.jump(None, self.constant(self.addr, REGISTER_TYPE))
+                self.jump(None, src)
         return src
 
     def negative(self, src, dst, ret):
